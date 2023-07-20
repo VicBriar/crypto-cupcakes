c708cbe3b050262b7d46e11c90dab67b49c8776e
Victoria McCrady <vmccrady@expediagroup.com>
2023-07-20 09:32:26 -0700

    setup auth

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
-// TODO - require express-openid-connect and destructure auth from it
+const {
+  SECRET,
+  AUDIENCE,
+  CLIENT,
+  BASE_URL
+} = process.env;
+const { auth } = require('express-openid-connect');
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: SECRET,
+  baseURL: AUDIENCE,
+  clientID: CLIENT,
+  issuerBaseURL: BASE_URL,
+}
 
 const { User, Cupcake } = require('./db');
 
@@ -13,21 +27,18 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(auth(config));
 
-/* *********** YOUR CODE HERE *********** */
-// follow the module instructions: destructure config environment variables from process.env
-// follow the docs:
-  // define the config object
-  // attach Auth0 OIDC auth router
-  // create a GET / route handler that sends back Logged in or Logged out
 
 app.get('/cupcakes', async (req, res, next) => {
-  try {
-    const cupcakes = await Cupcake.findAll();
-    res.send(cupcakes);
-  } catch (error) {
-    console.error(error);
-    next(error);
+  if(req.oidc.isAuthenticated()){
+    try {
+      const cupcakes = await Cupcake.findAll();
+      res.send(cupcakes);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
   }
 });
 
